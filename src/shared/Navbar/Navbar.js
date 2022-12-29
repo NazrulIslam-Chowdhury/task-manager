bff801e725679e815cbb8ca45d5e7aea8876ceed
rahiislam96 <nazrulislam66188@gmail.com>
2022-12-29 16:15:49 +0600

    logout implemented

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -1,10 +1,18 @@
 import { CButton, CCollapse, CContainer, CForm, CNavbar, CNavbarBrand, CNavbarNav, CNavbarToggler, CNavItem } from '@coreui/react';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import '@coreui/coreui/dist/css/coreui.min.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const Navbar = () => {
-    const [visible, setVisible] = useState(false)
+    const { user, logOut } = useContext(AuthContext);
+    const [visible, setVisible] = useState(false);
+
+    const logoutOnClick = () => {
+        logOut()
+            .then()
+            .catch(error => console.error(error))
+    }
     return (
         <>
             <CNavbar expand="lg" colorScheme="light" className="bg-light shadow-xl shadow-gray-700">
@@ -16,7 +24,7 @@ const Navbar = () => {
                         onClick={() => setVisible(!visible)}
                     />
                     <CCollapse className="navbar-collapse" visible={visible}>
-                        <CNavbarNav>
+                        <CNavbarNav className='flex items-center gap-1'>
                             <CNavItem>
                                 <Link className='text-gray-800 hover:text-gray-800 px-4 py-2 hover:bg-gray-300 font-normal font-serif no-underline' to="/" active>
                                     Home
@@ -33,11 +41,21 @@ const Navbar = () => {
                                     Completed Task
                                 </Link>
                             </CNavItem>
+                            <CNavItem>
+                                <img src={user?.photoURL} alt="" className='w-10 h-10 rounded-full' />
+                            </CNavItem>
                         </CNavbarNav>
-                        <CForm className="flex">
-                            <CButton type="submit" color="light" variant="outline">
-                                <Link className='text-black hover:text-black font-normal font-serif no-underline' to='/login'>Login</Link>
-                            </CButton>
+                        <CForm className="flex ml-1">
+                            {
+                                user?.uid ?
+                                    <CButton onClick={logoutOnClick} type="submit" color="light" variant="outline">
+                                        <Link className='text-black hover:text-black font-normal font-serif no-underline' to='/login'>Log Out</Link>
+                                    </CButton>
+                                    :
+                                    <CButton type="submit" color="light" variant="outline">
+                                        <Link className='text-black hover:text-black font-normal font-serif no-underline' to='/login'>Login</Link>
+                                    </CButton>
+                            }
                         </CForm>
                     </CCollapse>
                 </CContainer>
